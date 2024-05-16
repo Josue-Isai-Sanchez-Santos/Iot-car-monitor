5a602fdb537bd9769574b92421a15668b5f4a940
Josue-Isai-Sanchez-Santos <g17boom@gmail.com>
2024-05-15 20:32:09 -0600

    First Commit

---
+++ monitor/js/main.js
@@ -0,0 +1,34 @@
+alert("Hola Mundo");
+
+let Respuesta = document.getElementById("Respuesta");
+
+function CallApiRequest() {
+  // Hacer una petición para un usuario con ID especifico
+  axios
+    .get("http://localhost/iot-car-control/back-end/apis/getRegistro.php")
+    .then(function (response) {
+      // manejar respuesta exitosa
+      console.log(response);
+
+      let respuestaServidor =
+        response.data == "F"
+          ? "Adelante"
+          : response.data == "B"
+          ? "Atras"
+          : response.data == "L"
+          ? "Izquierda"
+          : response.data == "R"
+          ? "Derecha"
+          : "Detener";
+      Respuesta.innerHTML = "Respuesta: <strong>" + respuestaServidor + "</strong>";
+    })
+    .catch(function (error) {
+      // manejar error
+      console.log(error);
+    })
+    .finally(function () {
+      // siempre sera executado
+    });
+}
+
+setInterval(CallApiRequest, 2000);
